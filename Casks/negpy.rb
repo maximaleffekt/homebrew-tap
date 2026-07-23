ba0a7d26078e538f794d58466a416049670e0389
Maximaleffekt <max@freieslabor.org>
2026-07-23 17:59:45 +0200

    Added negpy.rb for version 0.41.0

---
+++ Casks/negpy.rb
@@ -0,0 +1,19 @@
+cask "negpy" do
+  version "0.41.0"
+  sha256 "7ff19c793ac9b71a2293c5ea76d83216ccb3c2aeb408eb20627d9604f8626d6c"
+
+  url "https://github.com/marcinz606/NegPy/releases/download/#{version}/NegPy-#{version}-macOS-arm64.dmg"
+  name "NegPy"
+  desc "Tool for processing film negatives"
+  homepage "https://github.com/marcinz606/NegPy"
+
+  depends_on arch: :arm64
+
+  app "NegPy.app"
+
+  zap trash: [
+    "~/Documents/NegPy/cache",
+    "~/Documents/NegPy/edits.db",
+    "~/Documents/NegPy/settings.db",
+  ]
+end
