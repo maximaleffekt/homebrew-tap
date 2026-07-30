e924987c13011135b4a60f8e78c33dab770ae962
Maximaleffekt <max@freieslabor.org>
2026-07-30 18:55:44 +0200

    updated negpy cask to version 0.45.0

--- Casks/negpy.rb
+++ Casks/negpy.rb
@@ -1,9 +1,9 @@
 cask "negpy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.44.0"
-  sha256 arm:   "8db21c20cff1e86e2457181aefd8b897ff023fa1068ecd2aefed1a77dccdd7a6",
-         intel: "0d24fb1f4d9e934da42368f6e4ef85a9ae432c8254893fd13cea852d2196b45e"
+  version "0.45.0"
+  sha256 arm:   "1cf8922f9130cd6d7e95e65c677d0c20c5047010597bf0597f1a69958dfb9782",
+         intel: "54314b97b93b122fb41ce79fdbb5a5e73eccff29761bd240caf95ec0179161f0"
 
   url "https://github.com/marcinz606/NegPy/releases/download/#{version}/NegPy-#{version}-macOS-#{arch}.dmg"
   name "NegPy"
